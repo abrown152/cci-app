095e7d3b7535550f5c70d0dc1499717f4e6ed8cb
Alysia Brown <alysiabrown@Alysias-MacBook-Pro.local>
2021-12-28 15:28:23 -0800

    Added system test for button click functionality.

--- test/system/welcomes_test.rb
+++ test/system/welcomes_test.rb
@@ -1,9 +1,11 @@
 require "application_system_test_case"
 
 class WelcomesTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test "should display new text" do
     visit root_path
 
-    assert_selector "h2", text: "CCI Tech Assessment App"
+    click_on "Click me"
+
+    assert_selector "p", text: "Nice job clicking that button!"
   end
 end
